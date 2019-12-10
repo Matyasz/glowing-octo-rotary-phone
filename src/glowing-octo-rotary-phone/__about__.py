0999b4b64b248036ddf591e87278f8c6d62ca0fa
Taylor Matyasz <matyasz@Taylors-MacBook-Pro.local>
2019-12-10 14:26:52 -0800

    initial commit

---
+++ src/glowing-octo-rotary-phone/__about__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__", "__email__"
+]
+
+__title__ = "pack"
+__summary__ = "MY first project!"
+__uri__ = ""
+
+__version__ = "1.0.0"
+
+__author__ = "Taylor Matyasz"
+__email__ = "tjmatyasz@gmail.com"
